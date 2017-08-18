8399216f336f6c9c21b11f3ca417cde24a582865
benwest <ben.s.west@gmail.com>
2017-08-18 17:37:15 +0200

    default port and ip

--- js/server.js
+++ js/server.js
@@ -4,4 +4,4 @@ var server = express();
 
 server.use( express.static( 'www' ) );
 
-server.listen( process.env.PORT, process.env.IP, () => console.log('👍🏻') );
+server.listen( process.env.PORT || 8000, process.env.IP || '127.0.0.1', () => console.log('👍🏻') );
